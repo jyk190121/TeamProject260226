287fb6e052a6f540518ebe2d156fe9cf9ab141e6
jyk190121 <46877384+jyk190121@users.noreply.github.com>
2026-02-26 12:42:29 +0900

    [JPGD-1] a초기화값 변경
    블라블라

--- Assets/Script/NewMonoBehaviourScript.cs
+++ Assets/Script/NewMonoBehaviourScript.cs
@@ -6,7 +6,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        a = 0;   
+        a = 1;
     }
 
     // Update is called once per frame
