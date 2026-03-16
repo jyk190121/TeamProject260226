7ba338743c2790dec53bdd7fbcdaec71800826da
nuhwork-kor <nuhwork2396@gmail.com>
2026-03-16 14:43:09 +0900

    Sprite 전면 교체

--- Assets/2. Script/Sound/SfxId.cs
+++ Assets/2. Script/Sound/SfxId.cs
@@ -22,10 +22,14 @@ public enum SfxId
     ZoomIn,
     ZoomOut,
 
+    //메인 1
+    Beach,
+
     //서브 1
     Cinderella_Clock,       //달 놓으면 시간 지나갔다는 째깍째깍 소리
     Cinderella_Deng,        //괘종시계 소리 (뎅- 뎅-)
     Cinderella_OpenDoor,    //달 놔서 시간가고 도망간다고 문열때
     Cinderella_Run,         //달리는소리 (잠깐 나올 예정)
+    HorseCarraige,          //말 달려가는 소리
 
 }
