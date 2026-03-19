9c13f7385e4ba10e14d956630bbef25a670faea4
kimjangw <kimjangw95@gmail.com>
2026-03-19 12:13:13 +0900

    확대존(ZoomZone) 문 연동 문제 해결
    - 확대 후 아무 작업 없이 바로 축소할 때 문이 다시 생겨나는 문제 디버깅 완료

--- Assets/2. Script/Manager/ItemManager.cs
+++ Assets/2. Script/Manager/ItemManager.cs
@@ -230,6 +230,14 @@ public class ItemManager : MonoBehaviour
 
     private void UpdateInteractZones(int chapter, int stage)
     {
+        //상태 확인 후 문 생성 조건 확인.
+        Item pumpkin = GetItemDataById("105");
+        bool isPumpkinUsed = (pumpkin != null && pumpkin.currentState == ItemState.Used);
+
+        Item clock = GetItemDataById("107");
+        bool hasClock = (clock != null && (clock.currentState == ItemState.Storage || clock.currentState == ItemState.Used));
+
+
         AnswerZone[] answerZones = Object.FindObjectsByType<AnswerZone>(FindObjectsInactive.Include, FindObjectsSortMode.None);
         foreach (var zone in answerZones)
         {
@@ -246,7 +254,20 @@ public class ItemManager : MonoBehaviour
         ObjectVisibilityController[] storyObjects = Object.FindObjectsByType<ObjectVisibilityController>(FindObjectsInactive.Include, FindObjectsSortMode.None);
         foreach (var obj in storyObjects)
         {
+            if ((isPumpkinUsed || hasClock) && obj.gameObject.name == "Stage1_Door")
+            {
+                obj.isSolved = true;
+                obj.gameObject.SetActive(false);
+                continue;
+            }
 
+            // 시계를 가졌다면 시계가 있던 배경(YesClock)은 무조건 비활성화
+            if (hasClock && obj.gameObject.name == "YesClock")
+            {
+                obj.isSolved = true;
+                obj.gameObject.SetActive(false);
+                continue;
+            }
 
             if (obj.isSolved) continue;
 
